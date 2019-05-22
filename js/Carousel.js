194e68a7f2ab21d89095659caca46911b59ea8bb
Jengopockets <jengopockets@gmail.com>
2019-05-22 14:00:57 -0400

    working on carousel

--- js/Carousel.js
+++ js/Carousel.js
@@ -3,43 +3,44 @@ class Carousel {
         this.carousel = element;
         this.rightBtn = this.carousel.querySelector('.right-button');
         this.leftBtn = this.carousel.querySelector('.left-button');
-        this.carouselItems = this.carousel.querySelectorAll('img');
-   
+        this.carouselItems = this.carousel.querySelectorAll('.dev1');
+        console.log(carouselItems);
+
         this.counter = 0;
         this.itemCount = Array.from(this.carouselItems).length;
-   
+
         this.current = this.carouselItems[0];
         this.current.style.display = 'block';
-   
+
         this.rightBtn.addEventListener('click', this.next.bind(this));
         this.leftBtn.addEventListener('click', this.prev.bind(this));
-       }
-        
-       prev(){
-           this.current.style.display = 'none';
-           if (this.counter === 0) {
-               this.counter = this.itemCount-1;
-               this.current = this.carouselItems[this.counter];
-               this.current.style.display = 'block';
-           } else {
-               this.counter -= 1;
-               this.current = this.carouselItems[this.counter];
-               this.current.style.display = 'block';
-           }
-       }
-   
-       next(){
-           this.current.style.display = 'none';
-           if (this.counter === this.itemCount-1) {
-               this.counter = 0;
-               this.current = this.carouselItems[this.counter];
-               this.current.style.display = 'block';
-           } else {
-               this.counter += 1;
-               this.current = this.carouselItems[this.counter];            this.current.style.display = 'block';
-           }
-       }
-   }
-   
-   let carousel = new Carousel(document.querySelector('.dev1'));
-   document.querySelector('.top-bar').style.zIndex = '1';
+    }
+
+    prev() {
+        this.current.style.display = 'none';
+        if (this.counter === 0) {
+            this.counter = this.itemCount - 1;
+            this.current = this.carouselItems[this.counter];
+            this.current.style.display = 'block';
+        } else {
+            this.counter -= 1;
+            this.current = this.carouselItems[this.counter];
+            this.current.style.display = 'block';
+        }
+    }
+
+    next() {
+        this.current.style.display = 'none';
+        if (this.counter === this.itemCount - 1) {
+            this.counter = 0;
+            this.current = this.carouselItems[this.counter];
+            this.current.style.display = 'block';
+        } else {
+            this.counter += 1;
+            this.current = this.carouselItems[this.counter];
+            this.current.style.display = 'block';
+        }
+    }
+}
+
+let carousel = new Carousel(document.querySelector('.carousel'));
